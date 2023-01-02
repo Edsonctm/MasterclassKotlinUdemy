4c02ae06fdff51e8a9442d0cc5149aa4b1928041
Edsonctm <Edson_ctm@hotmail.com>
2023-01-02 19:45:17 -0300

    .

---
+++ expressions/src/main/kotlin/main.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+}
+
+
+//val number = 15
+//val numbers = setOf(1,2,3,4,5,6,7,8,9,0)
+//
+//return if (number in numbers){
+//    println("$number faz parte da lista")
+//} else {
+//    println("$number não faz parte da lista")
+//}
+
+//val pet = "bird"
+//val petsDisponiveis = setOf("dog", "cat", "turtle", "rabbit")
+//
+//return if (pet in petsDisponiveis){
+//    println("I'm happy")
+//} else {
+//    println("I'm sad")
+//}
