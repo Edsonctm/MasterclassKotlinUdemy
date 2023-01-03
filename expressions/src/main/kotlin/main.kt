2a631516c95be4cef63bf5bf4ab98529fcf607a5
Edsonctm <Edson_ctm@hotmail.com>
2023-01-02 21:46:36 -0300

    .

--- expressions/src/main/kotlin/main.kt
+++ expressions/src/main/kotlin/main.kt
@@ -1,7 +1,45 @@
 fun main(){
+    
 
 }
 
+//println("Digite seu nome")
+//val nome = readLine()?:""
+//
+//when (nome[0]) {
+//    in 'a'..'c' -> println("Olá, enfermeira!")
+//    in 'd'..'f' -> println("Ohana!")
+//    else -> println("Não pensei em nada melhor")
+//}
+
+
+//println("Digite um numero de 1 a 12")
+//val numero = readLine()?:""
+//
+//when (numero.toInt()) {
+//    1 -> return println("Janeiro")
+//    2 -> return println("Fevereiro")
+//    3 -> return println("Março")
+//}
+
+//val listaConvidados = setOf("Raiane", "Carol", "Laiane")
+//println("Digite seu primeiro nome:")
+//val userName = readLine()?:""
+//
+//return if(userName in listaConvidados){
+//    println("Seja bem vinda")
+//} else {
+//    println("Você não foi convidada")
+//}
+
+
+//
+//val animais = arrayListOf<String>()
+//val animal = "Dog"
+//
+//if(animais.add(animal)){
+//    println("$animal foi adicionado")
+//}
 
 //val number = 15
 //val numbers = setOf(1,2,3,4,5,6,7,8,9,0)
