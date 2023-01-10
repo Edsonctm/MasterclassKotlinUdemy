299e05d81cfd50f08dadf30966f4d34c3dd93731
Edsonctm <Edson_ctm@hotmail.com>
2023-01-10 00:19:43 -0300

    Break Continue

---
+++ BreakContinue/src/main/kotlin/BreakContinue.kt
@@ -0,0 +1,51 @@
+fun main(){
+
+
+}
+
+//do{
+//    println("Digite sua idade:")
+//    val idade = readLine()?:""
+//    if (idade == "Stop"){
+//        println("Você encerrou o programa")
+//        break
+//    }
+//    if (idade.toInt() < 18){
+//        println("Menores de idade não são permitidos")
+//        continue
+//    }
+//    println("Seja bem vindo!")
+//} while (true)
+
+//loop@for(i in 1..4){
+//    for(j in 1..3){
+//        if (i % 3 == 0)
+//            continue@loop
+//        println("$i, $j")
+//    }
+//}
+
+//loop@for(i in 1..4){
+//    for(j in 1..3){
+//        if (i % 3 == 0)
+//            break@loop
+//        println("$i, $j")
+//    }
+//}
+
+//val numeros = arrayListOf(2,4,5,6,7,8,9,10,11,13)
+//
+//for (numero in numeros){
+//    if (numero % 2 == 0) continue
+//    println("$numero não é um número par!")
+//}
+
+
+//val numeros = arrayListOf(2,4,5,6)
+//
+//for (numero in numeros){
+//    if (numero % 2 == 1){
+//        println("$numero não é um número par!")
+//        break
+//    }
+//}
