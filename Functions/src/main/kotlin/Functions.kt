3457cc217bf562f8f323d0555b8d7e1d38ec9d90
Edsonctm <Edson_ctm@hotmail.com>
2023-01-12 00:14:38 -0300

    functions

--- Functions/src/main/kotlin/Functions.kt
+++ Functions/src/main/kotlin/Functions.kt
@@ -1,10 +1,53 @@
 fun main(){
-
-    ehPar(2)
-    
+    println(tamanho("camisa"))
+    println(tamanho(5))
 }
 
-fun ehPar(n: Int): Boolean{
-    if (n % 2 == 0) return true
-    return false
+fun tamanho(s: String): Int{
+    return s.length
 }
+
+fun tamanho(n: Int): String{
+    var retorno = ""
+    for (i in 0 .. n){
+        retorno += "a"
+    }
+
+    return retorno
+}
+
+
+
+
+//fun teste(n: String): String{
+//    return n
+//}
+//
+//fun teste(n: Int): Int{
+//    return n
+//}
+//
+//fun teste(n: Int?): Int{
+//    return n!!
+//}
+
+
+
+//fun lifespan(pet: String){
+//    var expectation = 0
+//    when (pet){
+//        "cat" -> expectation = 15
+//        "dog" -> expectation = 10
+//        "rabbit" -> expectation = 12
+//        else -> expectation = 20
+//    }
+//
+//    println("The lifespan estimated for a $pet is $expectation years!")
+//}
+
+
+
+//fun ehPar(n: Int): Boolean{
+//    if (n % 2 == 0) return true
+//    return false
+//}
